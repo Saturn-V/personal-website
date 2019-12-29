c260c8b732ff502b33c544fa54e10e9613cd0d31
Alex Aaron Pena <alexaaronpena@gmail.com>
2019-12-29 15:09:52 -0800

    Updates

---
+++ precache-manifest.b1c996e552f16c8affc8e16dceb10a03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88f50f839326624dcd9c2857b78135f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e692f230d42bb67c6654",
+    "url": "/static/css/main.d881f78a.chunk.css"
+  },
+  {
+    "revision": "7533efd2f2a754313a92",
+    "url": "/static/js/2.79ad69c8.chunk.js"
+  },
+  {
+    "revision": "e692f230d42bb67c6654",
+    "url": "/static/js/main.237929e2.chunk.js"
+  },
+  {
+    "revision": "bc995f61ca8cd94ca8b9",
+    "url": "/static/js/runtime~main.df3ff4b3.js"
+  }
+]);
